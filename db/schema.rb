442643b31f3d5a973fae2bfd333880d332330be1
Aaron Trostle <aaron.trostle@gmail.com>
2013-06-05 21:32:29 -0700

    Add Ticket feature via Rails scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130606043048) do
+
+  create_table "tickets", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
